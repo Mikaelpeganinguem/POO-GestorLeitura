8799951812b2b3c4febeaf5b2ca0e9e0661d6f70
Mikael Carlos <139816735+Mikaelpeganinguem@users.noreply.github.com>
2024-12-17 20:50:46 -0300

    Atualizar o funcionais.js

--- funcionais.js
+++ funcionais.js
@@ -83,100 +83,6 @@ function materialFactory(type, titulo, autor, inicio, termino) {
     return true;
 }
 
-function editData() {
-    let itemUser = parseInt(prompt("Digite o ID do material: "));
-    const index = materiais.findIndex(i => i.id === itemUser);
-
-    if (index !== -1) {
-        let title = prompt("Mudar título? (N/n): ").toLowerCase();
-        if (title === "n" || title.trim() === "") {
-            title = materiais[index].titulo;
-        }
-
-        let author = prompt("Mudar autor? (N/n): ").toLowerCase();
-        if (author === "n" || author.trim() === "") {
-            author = materiais[index].autor;
-        }
-
-        let typeMaterial = prompt("Mudar o Tipo de Material para: (N/n): ").toLowerCase();
-        if (typeMaterial === "n" || typeMaterial.trim() === "") {
-            typeMaterial = materiais[index].editTipoMaterial;
-        }
-
-        let startDate = prompt("Mudar data de início? (DD/MM/YYYY) (N/n): ");
-        if (startDate.toLowerCase() === "n" || startDate.trim() === "") {
-            startDate = materiais[index].editInicio;
-        } else {
-            while (!verifyDate(startDate)) {
-                startDate = prompt("Insira uma data válida: ");
-            }
-        }
-
-        let endDate = prompt("Mudar data de término? (DD/MM/YYYY) (N/n): ");
-        if (endDate.toLowerCase() === "n" || endDate.trim() === "") {
-            endDate = materiais[index].editFim;
-        } else {
-            while (!verifyDate(endDate)) {
-                endDate = prompt("Insira uma data válida: ");
-            }
-        }
-
-        materiais[index].editTitulo = title;
-        materiais[index].editAutor = author;
-        materiais[index].editTipoMaterial = typeMaterial;
-        materiais[index].editInicio = startDate;
-        materiais[index].editFim = endDate;
-
-        switch (typeMaterial) {
-            case 'livro':
-                let pag = prompt("Editar Número de páginas? (N/n): ").toLowerCase();
-                if (pag === "n" || pag.trim() === "") {
-                    pag = materiais[index].numPaginas;
-                } else {
-                    materiais[index].numPaginas = parseInt(pag);
-                }
-                break;
-
-            case 'artigo':
-                let volume = prompt("Editar Número do volume? (N/n): ").toLowerCase();
-                if (volume === "n" || volume.trim() === "") {
-                    volume = materiais[index].volume;
-                } else {
-                    materiais[index].volume = parseInt(volume);
-                }
-
-                let pagi = prompt("Editar Número de páginas? (N/n): ").toLowerCase();
-                if (pagi === "n" || pagi.trim() === "") {
-                    pagi = materiais[index].numPag;
-                } else {
-                    materiais[index].numPag = parseInt(pagi);
-                }
-
-                let peri = prompt("Editar Nome do Periódico? (N/n): ").toLowerCase();
-                if (peri === "n" || peri.trim() === "") {
-                    peri = materiais[index].periodico;
-                } else {
-                    materiais[index].periodico = peri;
-                }
-                break;
-
-            case 'revista':
-                let numEdic = prompt("Editar Número da edição? (N/n): ").toLowerCase();
-                if (numEdic === "n" || numEdic.trim() === "") {
-                    numEdic = materiais[index].numEdicao;
-                } else {
-                    materiais[index].numEdicao = parseInt(numEdic);
-                }
-                break;
-
-            default:
-                return "Tipo de material inválido!";
-        }
-        return "✅ Material atualizado com sucesso!";
-    } else {
-        return "❌ Material não encontrado!";
-    }
-}
 
 
 function search() {
@@ -216,4 +122,4 @@ function materialLido() {
     });
 }
 
-module.exports = { materiais, materialRegistration, search, remove, editData, materialLido };
+module.exports = { materiais, materialRegistration, search, remove, materialLido };
