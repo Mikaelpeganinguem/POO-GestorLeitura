7d3e85b4e65d8549bd7ee63f7020e52761af24ff
Mikael Carlos <139816735+Mikaelpeganinguem@users.noreply.github.com>
2024-12-18 08:22:41 -0300

    Update test.js

--- test.js
+++ test.js
@@ -1,7 +1,8 @@
 const Material = require('./classes/Material');
-//const Artigo = require('./classes/Artigo.js');
-//const Revista = require('./classes/Revista.js');
-//const Livro = require('./classes/Livro.js');
+const Artigo = require('./classes/Artigo.js');
+const Revista = require('./classes/Revista.js');
+const Livro = require('./classes/Livro.js');
+const { materiais, Revistas, Livros, Artigos } = require('./funcionais.js');
 
 
 const mat1 = new Material("Livro A", "João", "01/01/2023", "10/01/2023", "Livro");
@@ -10,9 +11,15 @@ const mat3 = new Material("Artigo C", "José", "10/01/2023", "15/01/2023", "Arti
 
 console.log(mat1.detalhes());
 console.log("");
+console.log(Livros.detalhes())
+console.log("")
 
 console.log(mat2.detalhes());
+console.log("");
+console.log(Revistas.detalhes());
 console.log("");
 
 console.log(mat3.detalhes());
 console.log("");
+console.log(Artigos.detalhes());
+console.log("")
